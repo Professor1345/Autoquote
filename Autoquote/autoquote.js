61d07b1cbd61051fa2c6bcf65935eeba7559f5af
Professor1345 <84884003+Professor1345@users.noreply.github.com>
2022-06-02 17:06:12 +0100

    Add files via upload

---
+++ Autoquote/autoquote.js
@@ -0,0 +1,101 @@
+function myQuote() {
+   
+var a = Math.floor(Math.random() * 20) + 1;
+// document.getElementById("demo").innerHTML = a;
+
+switch (a) {
+    case 1:
+        document.getElementById("q1").innerHTML=
+        "“We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein";
+        break;
+    case 2:
+        document.getElementById("q1").innerHTML = 
+        "“Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi";
+        break;
+    case 3:
+        document.getElementById("q1").innerHTML = 
+        "“Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.” — Mark Twain";
+        break;
+    case 4:
+        document.getElementById("q1").innerHTML=
+        "“When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.”— Eleanor Roosevelt";
+        break;
+    case 5:
+        document.getElementById("q1").innerHTML = 
+        "“When you change your thoughts, remember to also change your world.”—Norman Vincent Peale";
+        break;
+    case 6:
+        document.getElementById("q1").innerHTML = 
+        "“It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest. —Walter Anderson";
+        break;
+    case 7:
+        document.getElementById("q1").innerHTML=
+        "“Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.”—Diane McLaren";
+        break;
+    case 8:
+        document.getElementById("q1").innerHTML = 
+        "\"Success is not final; failure is not fatal: It is the courage to continue that counts.\" — Winston S. Churchill";
+        break;
+    case 9:
+        document.getElementById("q1").innerHTML = 
+        "\"It is better to fail in originality than to succeed in imitation.\" — Herman Melville";
+        break;
+    case 10:
+        document.getElementById("q1").innerHTML=
+        "\"The road to success and the road to failure are almost exactly the same.\" — Colin R. Davis";
+        break;
+    case 11:
+        document.getElementById("q1").innerHTML = 
+        "“Success usually comes to those who are too busy looking for it.” — Henry David Thoreau";
+        break;
+    case 12:
+        document.getElementById("q1").innerHTML = 
+        "“Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.” —Dale Carnegie";
+        break;
+    case 13:
+        document.getElementById("q1").innerHTML = 
+        "“Success is peace of mind, which is a direct result of self-satisfaction in knowing you made the effort to become the best of which you are capable.” —John Wooden";
+        break;
+    case 14:
+        document.getElementById("q1").innerHTML=
+        "“Success is getting what you want, happiness is wanting what you get.” ―W. P. Kinsella";
+        break;
+    case 15:
+        document.getElementById("q1").innerHTML = 
+        "“Don't let yesterday take up too much of today.” — Will Rogers";
+        break;
+    case 16:
+        document.getElementById("q1").innerHTML = 
+        "“You learn more from failure than from success. Don't let it stop you. Failure builds character.” — The Unknown";
+        break;
+    case 17:
+        document.getElementById("q1").innerHTML=
+        "“If you are working on something that you really care about, you don't have to be pushed. The vision pulls you.” — Steve Jobs";
+        break;
+    case 18:
+        document.getElementById("q1").innerHTML = 
+        "“Experience is a hard teacher because she gives the test first, the lesson afterwards.” ―Vernon Sanders Law";
+        break;
+    case 19:
+        document.getElementById("q1").innerHTML = 
+        "“The elevator to success is out of order. You’ll have to use the stairs, one step at a time.” — Joe Girard";
+        break;
+    case 20:
+        document.getElementById("q1").innerHTML=
+        "“Goal setting is the secret to a compelling future.” — Tony Robbins";
+        break;
+    default:
+        alert("Go and motivate yourself");
+        break;
+}
+//  if (a >= 10) {
+//     document.getElementById("q1").style.display = "block";
+//     // document.getElementById("q2").style.display = "none"
+
+//  } 
+//  if(a < 10) {
+//     document.getElementById("q2").style.display = "block";
+//     // document.getElementById("q1").style.display = "none";
+//  }
+}
+    
